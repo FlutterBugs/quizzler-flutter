ea46626a631b8dd17380583e056cf4d14770b735
Remzy527 <42874332+Remzy527@users.noreply.github.com>
2020-07-17 20:03:58 +0100

    Added a question class to reorganize the code and group the questions and answers together for cleaner code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -31,18 +32,16 @@ class _QuizPageState extends State<QuizPage> {
 
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
+  List<Question> questionBank = [
+    Question(questionText: 'You can lead a cow down stairs but not up stairs.', questionAnswer: false),
+    Question(questionText: 'Approximately one quarter of human bones are in the feet.', questionAnswer: true),
+    Question(questionText:'A slug\'s blood is green.', questionAnswer: true)
   ];
-
-  List<bool> answers = [false, true, true];
 
   int questionNumber = 0;
 
-  void validate(bool answer){
-    if (answers[questionNumber] == answer){
+  void checkAnswer(bool answer){
+    if (questionBank[questionNumber].questionAnswer == answer){
       scoreKeeper.add(correct);
     }
     else{
@@ -62,7 +61,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -88,7 +87,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  validate(true);
+                  checkAnswer(true);
 
                   questionNumber++;
                 });
@@ -111,7 +110,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  validate(false);
+                  checkAnswer(false);
                   questionNumber++;
                 });
               },
